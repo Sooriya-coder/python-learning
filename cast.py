ee8af8c367ff92a2193601b974b25b842b0a6740
sooriya-coder <sooriyapranyah@gmail.com>
2025-10-03 05:44:15 +0530

    code two

---
+++ cast.py
@@ -0,0 +1,17 @@
+a=5
+b=2.5
+c=1
+d=bool(0)
+e=bool(1)
+#gettint the data type using type() function
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+#converting the integer value into float using float() function
+print(float(a))
+#converting the float value into integer using int() function
+print(int(b))
+#converting the integer value into string value using str() function
+print(str(c))
